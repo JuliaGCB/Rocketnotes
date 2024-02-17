a5981540603cea474e82cde3ca75d155d218ebe9
julia <jujuba.gaby.barbosa@gmail.com>
2024-02-16 21:20:34 -0300

    Organizando as pastas

---
+++ pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+export function Details(){
+
+  return(
+    <>
+      <h1>Hello Julia</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, officia iusto. Fuga tempora autem, dignissimos ab vel doloremque sequi tempore laboriosam hic illum. Nisi minus nulla rerum esse, exercitationem illum.</p>
+    </>
+  )
+}
