f3b8fc8bc11c1d2ceffce893df43498b995684f3
julia <jujuba.gaby.barbosa@gmail.com>
2024-02-19 22:02:42 -0300

    Começando a estilização com oStyled Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Details } from '../pages/Details'
+import {ThemeProvider } from 'styled-components' // Prover um tema
+
+import { Details } from './pages/Details';
+import GlobalStyle from './styles/global'
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider theme ={theme}> 
+      <GlobalStyle/>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,12 @@
-import './style.css';
+import { Container } from './styles'
+
 
 export function Details(){
 
   return(
-    <>
+    <Container>
       <h1>Hello Julia</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, officia iusto. Fuga tempora autem, dignissimos ab vel doloremque sequi tempore laboriosam hic illum. Nisi minus nulla rerum esse, exercitationem illum.</p>
-    </>
+    </Container>
   )
 }
