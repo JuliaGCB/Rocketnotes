2c013ce40d55c887271a029753410247d642af1e
julia <jujuba.gaby.barbosa@gmail.com>
2024-02-22 20:23:42 -0300

    Começando o Header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,13 @@
 import { Container } from './styles'
 import { Button } from '../../components/Button'
+import { Header } from '../../components/Header'
 
 export function Details(){
 
   return(
     <Container>
-      <h1>Hello Word</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, officia iusto. Fuga tempora autem, dignissimos ab vel doloremque sequi tempore laboriosam hic illum. Nisi minus nulla rerum esse, exercitationem illum.</p>
-
-      <Button title="Entrar" loading/>
-      <Button title=" Cadastrar"/>
+      <Header />
+      
       <Button title="Voltar"/>
     </Container>
   )
