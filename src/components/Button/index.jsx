f83e779182d11d3a180b4cf3cfebd8add7f8c01c
julia <jujuba.gaby.barbosa@gmail.com>
2024-02-21 21:00:13 -0300

    Alterações no botão

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,13 @@
 import { Container } from "./styles";
 
-export function Button(){
+export function Button({title, loading = false, ...rest}){ //desestruturei o props e peguei direto o title
     return (
-        <Container type="button">
-            Meu Botão
+        <Container 
+            type="button"
+            disabled={loading}
+            {...rest} // Para colocar os componentes que eu crei mas que não coloquei aqui, ele ira puxar da mesma forma
+        >
+            { loading ? 'Carregando...' : title}
         </Container>
-    )
+    );
 }
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -8,7 +8,9 @@ export function Details(){
       <h1>Hello Word</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, officia iusto. Fuga tempora autem, dignissimos ab vel doloremque sequi tempore laboriosam hic illum. Nisi minus nulla rerum esse, exercitationem illum.</p>
 
-      <Button/>
+      <Button title="Entrar" loading/>
+      <Button title=" Cadastrar"/>
+      <Button title="Voltar"/>
     </Container>
   )
 }
