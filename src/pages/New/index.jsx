8691622175e4c6b2bdeed96577a211fcca2aaa88
julia <jujuba.gaby.barbosa@gmail.com>
2024-03-05 21:41:08 -0300

    Começando a pagina de criar nova nota
    Terminando de estilizar o profile
    Começando o New nota
    Fazendo o TextArea

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,25 @@
+import {Header} from '../../components/Header'
+import {Input } from '../../components/Input'
+import {Textarea} from '../../components/Textarea'
+
+import { Container, Form } from './style'
+
+export function New(){
+    return(
+        <Container>
+            <Header/>
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <a href="/">Voltar</a>
+                    </header>
+
+                    <Input placeholder="Título"/>
+                    <Textarea placeholder="Observações" />
+                </Form>
+            </main>
+        </Container>
+    )
+}
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,9 +1,9 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
-import { Container, Form } from './style';
+import { Container, Form, Avatar } from './style';
 
 export function Profile(){
     return(
@@ -15,6 +15,20 @@ export function Profile(){
             </header>
 
             <Form>
+                <Avatar>
+                    <img 
+                        src="https://github.com/JuliaGCB.png" 
+                        alt="Foto do usuário" 
+                    />
+                    <label htmlFor="avatar">
+                        <FiCamera/>
+
+                        <input 
+                            id='avatar'
+                            type='file'
+                        />
+                    </label>
+                </Avatar>
                 <Input
                     placeholder="Nome"  
                     type="text"
